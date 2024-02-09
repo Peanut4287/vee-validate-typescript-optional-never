526fd9b3c8e5dcce4d186d94245ae140bd1f1a27
Peanut4287 <157473901+Peanut4287@users.noreply.github.com>
2024-02-09 11:57:49 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { toTypedSchema } from '@vee-validate/yup';
+import { useForm } from 'vee-validate';
+import { ObjectSchema, number, object, string } from 'yup';
+
+interface User {
+  id?: number;
+  fullName: string;
+}
+
+const schema = toTypedSchema<ObjectSchema<User>>(
+  object({
+    id: number().optional(),
+    fullName: string().required(),
+  })
+);
+
+const { defineField } = useForm({
+  validationSchema: schema,
+});
+
+const [id] = defineField('id');
+const [fullName] = defineField('fullName');
+</script>
